ef61e9241d2b216e66b19c7d5e7593997b626a11
Sahasra Bezawada <itsmee.sahasra@gmail.com>
2026-09-25 14:28:41 -0500

    Renamed folder to Frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,6 +5,9 @@ function App() {
     <>
       <div className="flex items-center justify-center h-screen  bg-orange-100">
         <h1 className="text-4xl font-bold text-blue-500">Clario</h1>
+
+
+
       </div>
     </>
   )
